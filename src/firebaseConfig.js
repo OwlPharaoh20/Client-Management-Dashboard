4bf15cb0f9327e7daf5c45aa97ff0c1d6f8b5b00
GG-Pharaoh <oxdiablo20@gmail.com>
2024-11-09 05:48:48 -0800

    Auth related errors fixed, breezin now

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBqcr2eE8ijIrplLZZ5JsPMspQaCtcglMA",
   authDomain: "client-dashboard-b5c3d.firebaseapp.com",
   projectId: "client-dashboard-b5c3d",
   storageBucket: "client-dashboard-b5c3d.firebasestorage.app",
   messagingSenderId: "688172752022",
-  appId: "1:688172752022:web:078ad1df47c0dccd5676b7",
-  measurementId: "G-J4VBLG65DJ"
+  appId: "1:688172752022:web:078ad1df47c0dccd5676b7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and export it
+export const auth = getAuth(app);
